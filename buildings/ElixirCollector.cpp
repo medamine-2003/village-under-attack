c557ae87635c479bb3558defc4f49ba5d8c8abf0
medamine-2003 <medamine.jilani@etudiant-enit.utm.tn>
2025-04-26 01:04:01 +0100

    Update ElixirCollector.cpp

--- buildings/ElixirCollector.cpp
+++ buildings/ElixirCollector.cpp
@@ -1,15 +1,10 @@
 #include "ElixirCollector.h"
 
-ElixirCollector::ElixirCollector(Position pos)
-    : ResourceGenerator(3, 3,"🧪", 100, 0, 3, pos, 500) {} // Size 3x3, emoji 🧪, 100 gold, max 3, 500 health
-    // Note: Repr changes to 🧴 when full (handled in Board rendering)
+ElixirCollector::ElixirCollector(int x, int y) 
+    : ResourceGenerator(x, y, "🧪", 3, 3, 100, 3) {}
 
-bool ElixirCollector::collect(Resources& resources) {
-    if (current > 0) {
-        resources.addElixir(current);
-        current = 0;
-        isFull = false;
-        return true;
-    }
-    return false;
+int ElixirCollector::collect() {
+    int collected = current;
+    current = 0;
+    return collected;
 }
