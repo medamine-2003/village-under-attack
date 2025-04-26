20f387b77dbda6b4dfb3f7bca8ce2ce26539901e
medamine-2003 <medamine.jilani@etudiant-enit.utm.tn>
2025-04-26 01:05:44 +0100

    Update ResourceGenerator.cpp

--- buildings/ResourceGenerator.cpp
+++ buildings/ResourceGenerator.cpp
@@ -1,28 +1,14 @@
 #include "ResourceGenerator.h"
 
-ResourceGenerator::ResourceGenerator(int sizeX, int sizeY, std::string repr, int costGold, int costElixir, int maxInstances, Position pos, int health)
-    : Building(sizeX, sizeY, repr, costGold, costElixir, maxInstances, pos, health), current(0), capacity(100), isFull(false) {}
+ResourceGenerator::ResourceGenerator(int x, int y, const std::string& r, int sx, int sy, int c, int max)
+    : Building(x, y, r, sx, sy, c, max), current(0) {}
 
-void ResourceGenerator::generate() {
-    if (!isFull) {
-        current += 10; // Generate 10 units per cycle (adjust as needed)
-        if (current >= capacity) {
-            current = capacity;
-            isFull = true;
-        }
-    }
+bool ResourceGenerator::isFull() const {
+    return current >= capacity;
 }
 
-bool ResourceGenerator::collect(Resources& resources) {
-    if (current > 0) {
-        resources.addGold(current); // Default for GoldMine; overridden for ElixirCollector
-        current = 0;
-        isFull = false;
-        return true;
+void ResourceGenerator::update() {
+    if (!isFull()) {
+        current++;
     }
-    return false;
-}
-
-bool ResourceGenerator::isFullGenerator() const {
-    return isFull;
 }
