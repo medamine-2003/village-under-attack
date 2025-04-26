0872e4260288369ab3dd2b976a45b579b1700536
medamine-2003 <medamine.jilani@etudiant-enit.utm.tn>
2025-04-26 01:00:28 +0100

    Update Position.cpp

--- Position.cpp
+++ Position.cpp
@@ -1,6 +1,13 @@
 #include "Position.h"
+#include <cmath>
 
-Position::Position(int x, int y) : x(x), y(y) {}
+float Position::distanceTo(const Position& other) const {
+    return sqrt(pow(x - other.x, 2) + pow(y - other.y, 2));
+}
+
+Position Position::operator-(const Position& other) const {
+    return Position(x - other.x, y - other.y);
+}
 
 bool Position::operator==(const Position& other) const {
     return x == other.x && y == other.y;
