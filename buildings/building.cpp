a7a326b9c1d2450cd69f3501122f97100585f7b8
medamine-2003 <medamine.jilani@etudiant-enit.utm.tn>
2025-04-26 01:03:12 +0100

    Update building.cpp

--- buildings/building.cpp
+++ buildings/building.cpp
@@ -1,19 +1,32 @@
-#include "Building.h"
 
-Building::Building(int sizeX, int sizeY, std::string repr, int costGold, int costElixir, int maxInstances, Position pos, int health)
-    : sizeX(sizeX), sizeY(sizeY), repr(repr), costGold(costGold), costElixir(costElixir), maxInstances(maxInstances), position(pos), health(health) {}
+#include "building.h"
 
+Building::Building(int x, int y, const std::string& r, int sx, int sy, int c, int max)
+    : position(x, y), sizeX(sx), sizeY(sy), repr(r), cost(c), maxInstances(max), health(100) {}
+
+bool Building::isColliding(const Position& pos) const {
+    int left = position.x - sizeX/2;
+    int right = position.x + sizeX/2;
+    int top = position.y - sizeY/2;
+    int bottom = position.y + sizeY/2;
+
+    return (pos.x >= left && pos.x <= right && 
+            pos.y >= top && pos.y <= bottom);
+}
+
+void Building::takeDamage(int amount) {
+    health -= amount;
+}
+
+bool Building::isDestroyed() const {
+    return health <= 0;
+}
+
+// Getters implementation
+Position Building::getPosition() const { return position; }
 int Building::getSizeX() const { return sizeX; }
 int Building::getSizeY() const { return sizeY; }
 std::string Building::getRepr() const { return repr; }
-Position Building::getPosition() const { return position; }
+int Building::getCost() const { return cost; }
+int Building::getMaxInstances() const { return maxInstances; }
 int Building::getHealth() const { return health; }
-
-bool Building::canPlace(const Resources& resources) const {
-    return resources.gold >= costGold && resources.elixir >= costElixir;
-}
-
-void Building::takeDamage(int damage) {
-    if (damage > 0) health -= damage;
-    if (health < 0) health = 0;
-}
