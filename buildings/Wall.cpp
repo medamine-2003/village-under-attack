209046eb9676b90452aa8fc4df5cff53039ebac8
medamine-2003 <medamine.jilani@etudiant-enit.utm.tn>
2025-04-26 01:07:23 +0100

    Update Wall.cpp

--- buildings/Wall.cpp
+++ buildings/Wall.cpp
@@ -1,4 +1,8 @@
 #include "Wall.h"
 
-Wall::Wall(Position pos)
-    : Building(1, 1, "🚧", 10, 0, 200, pos, 300) {} // Size 1x1, emoji 🚧, 10 gold, max 200, 300 health
+Wall::Wall(int x, int y) 
+    : Building(x, y, "🧱", 1, 1, 10, 200) {}
+
+void Wall::update() {
+    // Wall doesn't need regular updates
+}
