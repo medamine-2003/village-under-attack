38a540c0f3cb7eaff07027c37a883b5338b05eb3
medamine-2003 <medamine.jilani@etudiant-enit.utm.tn>
2025-04-26 01:08:20 +0100

    Update Entity.cpp

--- Entities/Entity.cpp
+++ Entities/Entity.cpp
@@ -1,11 +1,16 @@
 #include "Entity.h"
 
-Entity::Entity(Position pos, std::string repr) : position(pos), repr(repr) {}
+Entity::Entity(int x, int y, const std::string& r, int h)
+    : position(x, y), repr(r), health(h) {}
+
+void Entity::takeDamage(int amount) {
+    health -= amount;
+}
+
+bool Entity::isDestroyed() const {
+    return health <= 0;
+}
 
 Position Entity::getPosition() const { return position; }
 std::string Entity::getRepr() const { return repr; }
-
-void Entity::move(int dx, int dy) {
-    position.x += dx;
-    position.y += dy;
-}
+int Entity::getHealth() const { return health; }
